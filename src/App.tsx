f834e27240349ab6fb13b28e74c50cde6d5540c2
Savindu Amalka <savinduamalka@gmail.com>
2025-08-15 16:31:22 +0530

    Set up routing and add login page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './authPages/login/page'
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
