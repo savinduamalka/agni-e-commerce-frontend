0c6c9261aaaf71c35383bfd5258a9519eb878182
Savindu Amalka <savinduamalka@gmail.com>
2025-08-15 17:23:49 +0530

    Wrap App and Toaster components with GoogleOAuthProvider for Google authentication

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'sonner'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+      <Toaster />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
