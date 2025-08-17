1e0594735e3a1ca55604970686b5febb2bef72ee
Savindu Amalka <savinduamalka@gmail.com>
2025-08-17 14:38:06 +0530

    Update routing: change login path to /signin and add routes for sign-up and email verification pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './authPages/signIn/page'
+import SignUpPage from './authPages/signUp/page'
+import VerifyEmailPage from './authPages/verify/page'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/verify" element={<VerifyEmailPage />} />
+        <Route path="/" element={<Navigate to="/signin" replace />} />
       </Routes>
     </BrowserRouter>
   )
