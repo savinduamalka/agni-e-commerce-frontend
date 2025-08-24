a76343aa5b0f6297e63cb15cf5840a8a186e2e4b
Savindu Amalka <savinduamalka@gmail.com>
2025-08-24 22:55:06 +0530

    Add Toaster component and update routing for Categories page

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import LoginPage from './authPages/signIn/page'
 import SignUpPage from './authPages/signUp/page'
 import VerifyEmailPage from './authPages/verify/page'
 import HomePage from './userPages/home/page'
+import CategoriesPage from './userPages/categories/page'
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster />
       <Routes>
         <Route path="/signin" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/verify" element={<VerifyEmailPage />} />
         <Route path="/" element={<HomePage/>} />
+        <Route path="/categories" element={<CategoriesPage />} />
       </Routes>
     </BrowserRouter>
   )
