fc03845834952ed5b62365215fee6755bde7755c
Savindu Amalka <savinduamalka@gmail.com>
2025-08-25 23:05:16 +0530

    Add jwt-decode dependency and refactor routing structure in App and main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import LoginPage from './authPages/signIn/page'
 import SignUpPage from './authPages/signUp/page'
 import VerifyEmailPage from './authPages/verify/page'
 import HomePage from './userPages/home/page'
 import CategoriesPage from './userPages/categories/page'
-import { Toaster } from "sonner";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Toaster />
       <Routes>
         <Route path="/signin" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
@@ -18,7 +15,6 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/categories" element={<CategoriesPage />} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'sonner'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <App />
-      <Toaster />
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+          <Toaster />
+        </AuthProvider>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
